913c0b5bf1965ecf5d309990c6c5a4434d3d84e5
mooyoni <hanmooyoni@gmail.com>
2025-04-03 23:31:50 +0900

    [INIT] java -> 17

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
-    // DB
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+
+	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
     // Test
